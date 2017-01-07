1fd267a362a1cfb39369f2d939d3f7e4a3c9161e
kukikoko <kukikoko@op.pl>
2017-01-07 11:32:27 +0100

    week4assgmnt

--- nauka_js/js/skrypt.js
+++ nauka_js/js/skrypt.js
@@ -6,7 +6,7 @@ function test1 (a) {
 		return a / 10;
 	}
 
-var b = 3 + 8;
+var b = 3 + test1(100);
 
 console.log (b);
 console.log (test1(150));
@@ -46,7 +46,48 @@ var sodom = {
 	warosc: 100
 	};
 
+for (var prop in sodom) {
+	console.log (prop + ": " + sodom[prop]);
+}
+
 console.log (sodom);
 
+function Circle (promien) {
+	this.promien = promien;
+}
 
+Circle.prototype.powierzchnia =
+	function () {
+		return Math.PI * Math.pow (this.promien, 2);
+	};
+
+var kolo = new Circle (2);
+console.log (kolo);
+console.log (kolo.powierzchnia());
+
+
+var Tabela = new Array ();
+Tabela [0] = "Pawel";
+Tabela [1] = 2;
+Tabela [2] = function (name) {
+	console.log ("Hello " + name);
+};
+Tabela [3] = {
+	kurs : "HTML" };
+
+b=0;
+
+
+for (b; b < Tabela.length; b = b+1) {
+	console.log (b + " -- " + Tabela[b]);
+}
+
+Tabela[2] (Tabela [0] + ", idz na kurs " +
+	Tabela [3].kurs);
+
+console.log (Tabela);
+
+// Imm Invoked Funct Expression
+
+(function() { console.log ("Brudny najbrudniejszy") })();
 
