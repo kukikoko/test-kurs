11af282f441ae1ecdb67a32f5a4d21ca3364fe49
kukikoko <kukikoko@op.pl>
2017-02-04 11:16:11 +0100

    ee

--- nauka_ng_3/js/skrypt.js
+++ nauka_ng_3/js/skrypt.js
@@ -16,7 +16,7 @@
 		tobuy.list = Checking.listOfThingsToBuy;
 
 		tobuy.gotit = function (which) {
-			Checking.change (which);
+			Checking.change (which, 1);
 		};
 	};
 
@@ -25,6 +25,11 @@
 	{
 		var bought = this;
 		bought.list = Checking.listOfBoughtThings;
+
+		bought.gotit = function (which)
+			{
+			Checking.change (which, 2);
+			};
 	};
 
 function Checking () {
@@ -40,10 +45,17 @@ function Checking () {
 
 	sr.listOfBoughtThings = new Array ();
 
-sr.change = function (number)
+sr.change = function (number, kierunek)
 	{
+	if (kierunek===1)
+		{
 		sr.listOfBoughtThings.push(sr.listOfThingsToBuy[number]);
 		sr.listOfThingsToBuy.splice (number, 1);
+		}
+	else {
+		sr.listOfThingsToBuy.push(sr.listOfBoughtThings[number]);
+		sr.listOfBoughtThings.splice (number, 1);
+		};
 	}
 };
 
