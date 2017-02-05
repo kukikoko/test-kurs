5ea356b87785205a10c48987996f30ce170968f6
kukikoko <kukikoko@op.pl>
2017-02-05 13:13:46 +0100

    ee

--- nauka_ng_3/js/skrypt.js
+++ nauka_ng_3/js/skrypt.js
@@ -31,14 +31,31 @@ function MenuSearchService ($http)
 {
 	var mss = this;
 
+
 	mss.GetMatchedMenuItems = function (content)
 		{
 			$http.get
 			("https://davids-restaurant.herokuapp.com/menu_items.json")
- .then(function successCallback(response) {
-	console.log (response);
-	return response.data }
-);
+ 		.then
+ 		(function successCallback(response) {
+
+		var matchedItems = new Array ();
+		var allItems = response.data;
+		var matching = content.toLowerCase();
+
+		for (var i = 0; i<allItems.length; i=i+1)
+		{
+			var desctription = allItems[i].desctription.toLowerCase();
+			if (desctription.indexOf(matching) !== -1)
+			{
+				matchedItems.push(allItems[i])
+			}
+		}
+
+		console.log (allItems);
+		console.log (matchedItems);
+		return matchedItems };
+		);
 		}
 
 };
