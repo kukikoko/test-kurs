bf3e020c388a96b41e0edb9dd0d0642d634486c3
kukikoko <kukikoko@op.pl>
2017-02-11 11:00:38 +0100

    e

---
+++ nauka_ng_4/js/skrypt.js
@@ -0,0 +1,70 @@
+(function () {
+	'use strict';
+
+	angular.module('Aplikacja', [])
+	.controller('Kontroler', Kontroler)
+	.service ('Serwis', Serwis)
+	.component ('mojKomponent', 
+		{
+			templateUrl: 'ins1.html',
+			controller: KontrolerKomponentu,
+			bindings:
+				{
+					znalezione: '<',
+					mojTytul: '@tytul',
+					wywalTo: '&'
+				}
+		});
+
+
+	function KontrolerKomponentu ()
+		{
+			$ctrl = this;
+		}
+
+
+	Kontroler.$inject = 
+	['Serwis'];
+	function Kontroler (Serwis)
+	{
+		var knt = this;
+		knt.pozycja = "";
+		knt.ilosc = "";
+		knt.lista = Serwis.menu;
+
+		knt.dodaj = function ()
+			{
+				Serwis.wpisz (knt.pozycja, knt.ilosc);
+				knt.pozycja = "";
+				knt.ilosc = "";
+				console.log (knt.lista);
+			};
+
+	};
+
+
+
+
+function Serwis () 
+{
+	var srw = this;
+	srw.menu = new Array ();
+
+	srw.wpisz = function (co, ile)
+		{
+			var pozycja = {
+				nazwa: co,
+				ilosc: ile
+			};
+
+			if (co != "")
+				{
+					srw.menu.push (pozycja);
+				};
+		};
+
+};
+
+
+
+})();
