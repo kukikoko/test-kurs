4b6a6e0f3a4fa9c7dad610bd95c9fbe62240cae6
kukikoko <kukikoko@op.pl>
2017-02-12 06:54:32 +0100

    e

--- tegojego/js/skrypt.js
+++ tegojego/js/skrypt.js
@@ -21,33 +21,33 @@
 		{
 			var $ctrl = this;
 			var wyswietlamy;
-			var ListaZapasowa;
-			$ctrl.ListaZamowienia = [];
+			$ctrl.ListaZapasowa = [];
 			$ctrl.cena = 0;
 
 
 			$ctrl.reseta = function ()
 				{
 					console.log ("A HUJA TAM!");
-					for (var i=0; i<ListaZapasowa.length; i=i-1)
+					$ctrl.ListaZapasowa.length = 0;
+					for (i=0; i<$ctrl.rzeczy.length; i++)
 					{
-						console.log ("PIZDA");
+						$ctrl.ListaZapasowa.push($ctrl.rzeczy[i]);
 					}
+					wyswietlamy = 1;
 				};
 
 
 			$ctrl.zamow = function ()
 				{
-					if ($ctrl.rzeczy.length > 0)
+					if ($ctrl.ListaZapasowa.length > 0)
 					{
-					for (var i=$ctrl.rzeczy.length; i>0; i=i-1)
+					var sumowanie = 0;
+					for (var i=0; i<$ctrl.ListaZapasowa.length; i=i+1)
 						{
-							$ctrl.ListaZamowienia.push($ctrl.rzeczy[i-1]);
-							$ctrl.remove (i-1);
+							sumowanie =
+							sumowanie+$ctrl.ListaZapasowa[i].cena;
 						};
-
-					$ctrl.cena=Math.round
-					($ctrl.ListaZamowienia.length * 100)/100;
+					$ctrl.cena = sumowanie;
 					wyswietlamy = 2;
 					}
 					else
@@ -57,13 +57,17 @@
 			$ctrl.$onInit = function ()
 				{
 					console.log ("ON INIT");
-					wyswietlamy = 1;
-					ListaZapasowa = $ctrl.rzeczy;
+					$ctrl.reseta ();
 				};
 
 			$ctrl.$onChanges = function (changeObj)
 				{
 					console.log ("changes: ",changeObj);
+				}
+
+			$ctrl.won = function (co)
+				{
+					$ctrl.ListaZapasowa.splice(co, 1);
 				}
 
 			$ctrl.remove = function (myIndex) 
@@ -81,7 +85,7 @@
 								var Elem3 = $element.find('.blad');
 								Elem1.slideDown(900);
 								Elem2.slideUp(900);
-								Elem3.slideup(900);
+								Elem3.slideUp(900);
 							}
 					else
 					{
@@ -134,16 +138,17 @@
 		{
 			var s = this;
 			s.menu = [
-			{nazwa: "Lubię Paczeć", ilosc: "2015"},
-			{nazwa: "Tego Jego", ilosc: "2014"},
-			{nazwa: "Los Iwiczna Locos", ilosc: "2014"},
-			{nazwa: "Gestapowski Płaszcz Życia", ilosc: "2017"},
-			{nazwa: "Skrzydła z Tłuszczu", ilosc: "2017"},
-			{nazwa: "Pogwałcono Prawa Me", ilosc: "2018"},
-			{nazwa: "Dintojra", ilosc: "2018"},
-			{nazwa: "Kiedy Idę Najebany (ulicami mego miasta w dzień)", ilosc: "2019"},
-			{nazwa: "Eksmisja Komornicza", ilosc: "2019"},
-			{nazwa: "Zimmmmmmno Mu", ilosc: "2019"}];
+			{nazwa: "Lubię Paczeć", ilosc: "2015", cena: 3.40},
+			{nazwa: "Tego Jego", ilosc: "2014", cena: 3.22},
+			{nazwa: "Los Iwiczna Locos", ilosc: "2014", cena: 2.89},
+			{nazwa: "Gestapowski Płaszcz Życia", ilosc: "2017", cena: 4.43},
+			{nazwa: "Skrzydła z Tłuszczu", ilosc: "2017", cena: 3.99},
+			{nazwa: "Pogwałcono Prawa Me", ilosc: "2018", cena: 1.87},
+			{nazwa: "Dintojra", ilosc: "2018", cena: 2.33},
+			{nazwa: "Kiedy Idę Najebany (ulicami mego miasta w dzień)", ilosc: "2019", cena: 5.23},
+			{nazwa: "Eksmisja Komornicza", ilosc: "2019", cena: 3.43},
+			{nazwa: "Zimmmmmmno Mu", ilosc: "2019", cena: 2.97},
+			{nazwa: "Krójże Mnie Tasakiem Losu", ilosc: "2020", cena: 8.32}];
 
 			s.sprawdz = function (co)
 				{
